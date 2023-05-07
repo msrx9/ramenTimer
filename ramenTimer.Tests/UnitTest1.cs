2888786ffdef38df3b3224fda3c7e93dc7a418fc
msrx9 <msrx9g@gmail.com>
2023-05-07 15:58:09 +0900

    フラグ関係の処理のリファクタリング及びテスト追加

--- ramenTimer.Tests/UnitTest1.cs
+++ ramenTimer.Tests/UnitTest1.cs
@@ -2,10 +2,59 @@
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Fact(DisplayName = "ToggleFlag(): TrueをFalseにトグル")]
+    public void ToggleFlag_ShouldSetFlagTrueToFalse()
     {
-        var target = new MyClass();
-        Assert.Equal(1, target.MyMethod());
+        // Arrange
+        bool flag = true;
+
+        // Act
+        var target = new FlagControl();
+
+        // Assert
+        Assert.False(target.Toggle(flag));
+    }
+
+    [Fact(DisplayName = "ToggleFlag(): FalseをTrueにトグル")]
+    public void ToggleFlag_ShouldSetFlagFalseToTrue()
+    {
+        // Arrange
+        bool flag = false;
+
+        // Act
+        var target = new FlagControl();
+
+        // Assert
+        Assert.True(target.Toggle(flag));
+    }
+
+    [Fact(DisplayName = "IsTimeout(): タイムアップ")]
+    public void IsTimeoutTrue_ShouldReturnTrue()
+    {
+        // Arrange
+        TimeOnly timer = new TimeOnly(1, 0);
+
+        // Act
+        var test = new FlagControl();
+        var ret = test.IsTimeout(timer);
+
+        // Assert
+        Assert.True(ret);
+    }
+
+    [Fact(DisplayName = "IsTimeout(): カウント中")]
+    public void IsTimeoutFalse_ShouldReturnTrue()
+    {
+        // Arrange
+        TimeOnly timer = new TimeOnly(0, 0);
+        int milliseconds = 1;
+        timer = timer.Add(TimeSpan.FromMilliseconds(milliseconds));
+
+        // Act
+        var test = new FlagControl();
+        var ret = test.IsTimeout(timer);
+
+        // Assert
+        Assert.False(ret);
     }
 }
---
+++ ramenTimer/FlagControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ramenTimer
+{
+	public class FlagControl: IFlagControl
+	{
+        public bool IsTimeout(TimeOnly leftTime)
+        {
+            if ((leftTime.Minute | leftTime.Second | leftTime.Millisecond) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Toggle(bool flag)
+        {
+            return !flag;
+        }
+    }
+}
